b870aa016a58f63c174bbee0a51a2c8ce8aebff2
soulgalore <peter@soulgalore.com>
2020-02-28 17:48:50 +0100

    full path

--- src/hostingDatabase.test.js
+++ src/hostingDatabase.test.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const hosting = require('./hostingDatabase');
+const path = require('path');
 
 describe('hostingDatabase', function () {
  
   describe('checking a single domain with #check', function () {
     it("tries to use a local database if available ", async function () {
-      const res = await hosting.check("google.com", "../url2green.test.db")
+      const res = await hosting.check("google.com",  path.resolve(__dirname, "..", "url2green.test.db"))
       expect(res).toEqual(true)
     })
   })
   describe('implicitly checking multiple domains with #check', function () {
     it("tries to use a local database if available", async function () {
-      const res = await hosting.check(["google.com", "kochindustries.com"], "../url2green.test.db")
+      const res = await hosting.check(["google.com", "kochindustries.com"], path.resolve(__dirname, "..", "url2green.test.db"))
       expect(res).toContain("google.com")
     })
   })
