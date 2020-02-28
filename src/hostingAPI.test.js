94543175adb117761a2b6cd4b672e4686a39e1df
soulgalore <peter@soulgalore.com>
2020-02-28 19:14:39 +0100

    updated test text

--- src/hostingAPI.test.js
+++ src/hostingAPI.test.js
@@ -4,14 +4,14 @@ const hosting = require('./hostingAPI');
 
 describe('hostingAPI', function () {
   describe('checking a single domain with #check', function () {
-    it("falls back to using the API to check instead", async function () {
+    it("using the API", async function () {
       const res = await hosting.check("google.com")
       expect(res).toEqual(true)
     })
 
   })
   describe('implicitly checking multiple domains with #check', function () {
-    it("falls back to the API when no db is present", async function () {
+    it("using the API", async function () {
       const res = await hosting.check(["google.com", "kochindustries.com"])
       expect(res).toContain("google.com")
     })
