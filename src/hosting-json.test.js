f6f571258ec30ac354b4d8280a93f4c7968d6514
Chris Adams <chris@productscience.co.uk>
2020-03-14 19:21:52 +0100

    Move the loading of the JSON out of the call to check
    So we can pass the db in repeatedly, without loading every time

--- src/hosting-json.test.js
+++ src/hosting-json.test.js
@@ -2,19 +2,23 @@
 
 const hosting = require("./hosting-json");
 const path = require("path");
-const jsonPath = path.resolve(__dirname, "..", "url2green.json");
 
 describe("hostingJSON", function() {
+  const jsonPath = path.resolve(__dirname, "..", "url2green.json");
+
   describe("checking a single domain with #check", function() {
     test("against the list of domains as JSON", async function() {
-      const res = await hosting.check("google.com", jsonPath);
+      const db = await hosting.loadJSON(jsonPath);
+      const res = await hosting.check("google.com", db);
       expect(res).toEqual(true);
     });
   });
   describe("implicitly checking multiple domains with #check", function() {
     test("against the list of domains as JSON", async function() {
+      const db = await hosting.loadJSON(jsonPath);
       const domains = ["google.com", "kochindustries.com"];
-      const res = await hosting.check(domains, jsonPath);
+
+      const res = await hosting.check(domains, db);
       expect(res).toContain("google.com");
     });
   });
