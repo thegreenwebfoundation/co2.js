b316845a4924167270dd5f53fb9caca37e375d63
soulgalore <peter@soulgalore.com>
2021-11-22 03:35:40 +0100

    Read JSON blob also as gzipped.

--- src/hosting-json.test.js
+++ src/hosting-json.test.js
@@ -12,9 +12,24 @@ describe("hostingJSON", function() {
     "url2green.test.json"
   );
 
+  const jsonPathGz = path.resolve(
+    __dirname,
+    "..",
+    "data",
+    "fixtures",
+    "url2green.test.json.gz"
+  );
+
   describe("checking a single domain with #check", function() {
     test("against the list of domains as JSON", async function() {
       const db = await hosting.loadJSON(jsonPath);
+      const res = await hosting.check("google.com", db);
+      expect(res).toEqual(true);
+    });
+  });
+  describe("checking a single domain with #check", function() {
+    test("against the list of domains as JSON loaded from a gzipped JSON", async function() {
+      const db = await hosting.loadJSON(jsonPathGz);
       const res = await hosting.check("google.com", db);
       expect(res).toEqual(true);
     });
