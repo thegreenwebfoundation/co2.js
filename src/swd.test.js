8c84043c62760b46a84c45ac795ade1ad252cf61
drydenwilliams <hello@drydenwilliams.co.uk>
2021-12-16 14:41:13 +0000

    feature: add sustainable web design modle

---
+++ src/swd.test.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path');
+const SWD = require('./swd');
+
+describe('swd', () => {
+  let har, swd;
+  const averageWebsiteInByts = 2257715.2;
+
+  beforeEach(() => {
+    swd = new SWD();
+    har = JSON.parse(
+      fs.readFileSync(
+        path.resolve(__dirname, '../data/fixtures/tgwf.har'),
+        'utf8'
+      )
+    );
+  });
+
+  describe('energyPerVisit', function () {
+    it('should return a number', () => {
+      expect(typeof swd.energyPerVisit(2257715.2)).toBe('number');
+    });
+
+    it('should calculate the correct energy', () => {
+      expect(swd.energyPerVisit(2257715.2)).toBe(0.0012858824157714846);
+    });
+  });
+
+  describe('emissionsPerVisitInGrams', function () {
+    it('should caulate the correct co2 per visit', () => {
+      const averageWebsiteInByts = 2257715.2;
+      const energy = swd.energyPerVisit(averageWebsiteInByts);
+      expect(swd.emissionsPerVisitInGrams(energy)).toEqual(0.61);
+    });
+  });
+
+  describe('annualEnergyInKwh', function () {
+    it('should calculate the correct energy in kWh', () => {
+      expect(swd.annualEnergyInKwh(averageWebsiteInByts)).toBe(27092582400);
+    });
+  });
+
+  describe('annualEmissionsInGrams', function () {
+    it('should calculate the corrent energy in grams', () => {
+      expect(swd.annualEmissionsInGrams(averageWebsiteInByts)).toBe(
+        27092582400
+      );
+    });
+  });
+
+  describe('annualSegmentEngergy', function () {
+    it('should return the correct values', () => {
+      expect(swd.annualSegmentEngergy(averageWebsiteInByts)).toEqual({
+        consumerDeviceEnergy: 1174011.9,
+        dataCenterEnergy: 338657.28,
+        networkEnergy: 316080.13,
+        productionEnergy: 428965.89,
+      });
+    });
+  });
+});
