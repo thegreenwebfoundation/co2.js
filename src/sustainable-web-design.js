1956ea8f039b100894853872fd72497a90593c68
drydenwilliams <hello@drydenwilliams.co.uk>
2022-03-09 18:12:02 +0000

    refactor: fix typos, update energy variables and order, add reference comments

--- src/sustainable-web-design.js
+++ src/sustainable-web-design.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Sustainable Web Design
@@ -8,14 +8,19 @@
  *
  *
  */
-const { fileSize } = require("./constants");
-const { formatNumber } = require("./helpers");
+const { fileSize } = require('./constants');
+const { formatNumber } = require('./helpers');
 
+// Taken from: https://sustainablewebdesign.org/calculating-digital-emissions/#:~:text=TWh/EB%20or-,0.81%20kWH/GB,-Carbon%20factor%20(global
 const KWH_PER_GB = 0.81;
-const GLOABL_INTENSITY = 475;
+// Taken from: https://sustainablewebdesign.org/calculating-digital-emissions/#:~:text=intensity%20of%20electricity%20(-,475g/kWh,-).%C2%A0
+const GLOBAL_INTENSITY = 475;
+// Taken from: https://gitlab.com/wholegrain/carbon-api-2-0/-/blob/master/includes/carbonapi.php
 const FIRST_TIME_VIEWING_PERCENTAGE = 0.25;
 const RETURNING_VISITOR_PERCENTAGE = 0.75;
 const PERCENTAGE_OF_DATA_LOADED_ON_SUBSEQUENT_LOAD = 0.02;
+// Taken from: https://sustainablewebdesign.org/calculating-digital-emissions/#:~:text=Consumer%20device%20energy%20%3D%20AE%20x%200.52
+const END_USER_DEVICE_ENERGY = 0.52;
 
 class SustainableWebDesign {
   constructor(options) {
@@ -24,16 +29,19 @@ class SustainableWebDesign {
 
   energyPerVisit(bytes) {
     const transferedBytesToGb = bytes / fileSize.GIGABYTE;
-    return (
-      transferedBytesToGb * KWH_PER_GB * RETURNING_VISITOR_PERCENTAGE +
+
+    const newVisitorEnergy =
+      transferedBytesToGb * KWH_PER_GB * FIRST_TIME_VIEWING_PERCENTAGE;
+    const returningVisitorEnergy =
       transferedBytesToGb *
-        KWH_PER_GB *
-        FIRST_TIME_VIEWING_PERCENTAGE *
-        PERCENTAGE_OF_DATA_LOADED_ON_SUBSEQUENT_LOAD
-    );
+      KWH_PER_GB *
+      RETURNING_VISITOR_PERCENTAGE *
+      PERCENTAGE_OF_DATA_LOADED_ON_SUBSEQUENT_LOAD;
+
+    return newVisitorEnergy + returningVisitorEnergy;
   }
 
-  emissionsPerVisitInGrams(energyPerVisit, globalIntensity = GLOABL_INTENSITY) {
+  emissionsPerVisitInGrams(energyPerVisit, globalIntensity = GLOBAL_INTENSITY) {
     return formatNumber(energyPerVisit * globalIntensity);
   }
 
@@ -45,9 +53,9 @@ class SustainableWebDesign {
     return co2grams * monthlyVisitors * 12;
   }
 
-  annualSegmentEngergy(annualEnergy) {
+  annualSegmentEnergy(annualEnergy) {
     return {
-      consumerDeviceEnergy: formatNumber(annualEnergy * 0.52),
+      consumerDeviceEnergy: formatNumber(annualEnergy * END_USER_DEVICE_ENERGY),
       networkEnergy: formatNumber(annualEnergy * 0.14),
       dataCenterEnergy: formatNumber(annualEnergy * 0.15),
       productionEnergy: formatNumber(annualEnergy * 0.19),
