9659653a05c0c8febd0953ce118a317ab0d39b26
Chris Adams <chris@productscience.co.uk>
2022-03-31 23:15:43 +0200

    Update tests, move logic from CO2 to 1byte model

--- src/hosting-api.test.js
+++ src/hosting-api.test.js
@@ -3,9 +3,9 @@
 const hosting = require("./hosting-api");
 const nock = require("nock");
 
-describe("hostingAPI", function () {
-  describe("checking a single domain with #check", function () {
-    it("using the API", async function () {
+describe("hostingAPI", () => {
+  describe("checking a single domain with #check", () => {
+    it("using the API", async () => {
       const scope = nock("https://api.thegreenwebfoundation.org/")
         .get("/greencheck/google.com")
         .reply(200, {
@@ -16,8 +16,8 @@ describe("hostingAPI", function () {
       expect(res).toEqual(true);
     });
   });
-  describe("implicitly checking multiple domains with #check", function () {
-    it("using the API", async function () {
+  describe("implicitly checking multiple domains with #check", () => {
+    it("using the API", async () => {
       const scope = nock("https://api.thegreenwebfoundation.org/")
         .get("/v2/greencheckmulti/[%22google.com%22,%22kochindustries.com%22]")
         .reply(200, {
--- src/sustainable-web-design.js
+++ src/sustainable-web-design.js
@@ -16,17 +16,48 @@ const KWH_PER_GB = 0.81;
 // Taken from: https://ember-climate.org/data/data-explorer
 // - Global carbon intensity for 2021
 
+
+
 const GLOBAL_INTENSITY = 442;
+// TODO add proper weighted average for non co2 emitting sources,
+// like wind, solar, hydro
+const RENEWABLES_INTENSITY = 0
+
+
 // Taken from: https://gitlab.com/wholegrain/carbon-api-2-0/-/blob/master/includes/carbonapi.php
 const FIRST_TIME_VIEWING_PERCENTAGE = 0.25;
 const RETURNING_VISITOR_PERCENTAGE = 0.75;
 const PERCENTAGE_OF_DATA_LOADED_ON_SUBSEQUENT_LOAD = 0.02;
 // Taken from: https://sustainablewebdesign.org/calculating-digital-emissions/#:~:text=Consumer%20device%20energy%20%3D%20AE%20x%200.52
+
 const END_USER_DEVICE_ENERGY = 0.52;
+const NETWORK_ENERGY = 0.14;
+const DATACENTER_ENERGY = 0.15
+const PRODUCTION_ENERGY = 0.19
+
 
 class SustainableWebDesign {
   constructor(options) {
     this.options = options;
+  }
+
+
+  /**
+   * Accept a figure for bytes transferred and return a figure for CO2
+   * emissions. If transfer is for a domain served by renewables, return
+   * a figure to account for this
+   *
+   * @param {Number} bytes
+   * @param {boolean} green
+   * @return {Number}
+   */
+  perByte(bytes, green) {
+    const transferedBytesToGb = bytes / fileSize.GIGABYTE;
+    const energyUsage = transferedBytesToGb * KWH_PER_GB
+    return energyUsage * GLOBAL_INTENSITY
+
+
+
   }
 
   energyPerVisit(bytes) {
@@ -58,9 +89,9 @@ class SustainableWebDesign {
   annualSegmentEnergy(annualEnergy) {
     return {
       consumerDeviceEnergy: formatNumber(annualEnergy * END_USER_DEVICE_ENERGY),
-      networkEnergy: formatNumber(annualEnergy * 0.14),
-      dataCenterEnergy: formatNumber(annualEnergy * 0.15),
-      productionEnergy: formatNumber(annualEnergy * 0.19),
+      networkEnergy: formatNumber(annualEnergy * NETWORK_ENERGY),
+      dataCenterEnergy: formatNumber(annualEnergy * DATACENTER_ENERGY),
+      productionEnergy: formatNumber(annualEnergy * PRODUCTION_ENERGY),
     };
   }
 }
