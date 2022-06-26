8a04668b599578dd2da2bc0d31aa883f09668d1f
Chris Adams <chris@productscience.co.uk>
2022-06-26 22:34:28 +0200

    Add esbuild configs for different runtimes

---
+++ .esbuild.browser.js
@@ -0,0 +1,11 @@
+require('esbuild').buildSync({
+  entryPoints: ['src/index.js'],
+  outdir: 'public',
+  globalName: 'co2',
+  format: 'iife',
+  platform: 'browser',
+  bundle: true,
+  sourcemap: true,
+  minify: true,
+  // target: ['es5'],
+})
