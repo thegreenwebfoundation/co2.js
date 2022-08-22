755854506e7a456a44454c6674ae94dd19c4d8dd
Fershad <itsfish@fershad.com>
2022-08-22 14:50:52 +0800

    remove unused import

--- src/co2.test.js
+++ src/co2.test.js
@@ -6,7 +6,6 @@ import path from "path";
 import pagexray from "pagexray";
 
 import CO2 from "./co2.js";
-import SustainableWebDesign from "./sustainable-web-design.js";
 
 describe("co2", () => {
   let har, co2;
