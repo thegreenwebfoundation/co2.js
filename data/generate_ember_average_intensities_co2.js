ccac167e953db6fbd028a6f74c0dc0aff35bd969
Fershad <itsfish@fershad.com>
2022-09-08 16:24:41 +0800

    add comments to code

--- data/generate_ember_average_intensities_co2.js
+++ data/generate_ember_average_intensities_co2.js
@@ -9,9 +9,9 @@ const array = csv.toString().split("\n");
 
 /* Store the converted result into an array */
 const csvToJsonResult = {};
-const gridIntensityResults = {}
+const gridIntensityResults = {};
 
-/* Store the CSV column headers into seprate variable */
+/* Store the CSV column headers into separate variable */
 const headers = getHeaders(array[0]);
 
 /* Iterate over the remaining data rows */
@@ -22,37 +22,58 @@ for (let currentArrayString of array) {
 
 	/* Empty object to store result in key value pair */
 	const jsonObject = {}
+
 	/* Store the current array element */
 	let string = ''
 	let quoteFlag = 0
+
+	// Iterate over the current array element and generate a string
 	for (let character of currentArrayString) {
+
+
+		// The next set of if statements helps setup the current array element to be split by columns (comma delimited)
+		// However, some textual cells may contain commas, so we need to check for those and ignore them.
+		// We do this by looking for textual cells that are enclosed in double quotes, and setting the quoteFlag to 1 when we encounter one.
 		if (character === '"' && quoteFlag === 0) {
 			quoteFlag = 1
-		} else if (character === '"' && quoteFlag == 1) quoteFlag = 0
+		} else if (character === '"' && quoteFlag == 1) {
+			quoteFlag = 0
+		}
+
+		// If we encounter a comma, and the quoteFlag is 0, then we know that we have reached the end of a column. We replace the comma with a pipe character to help us split the string later.
 		if (character === ',' && quoteFlag === 0) character = '|'
 		if (character !== '"') string += character
 	}
 
+	// Split the string by the pipe character
 	let jsonProperties = string.split("|")
+
+	// Ember sets the country value (3-digit country code) in the first column. If data is for a region instead, it will be in the second column.
+	// We can assign the current country (or region) by using these fields.
 	let country = jsonProperties[0] || jsonProperties[1];
 
-	for (let j in headers) {
-		if (jsonProperties[j].includes(",")) {
-			jsonObject[headers[j]] = jsonProperties[j]
+	// Loop through the headers and assign the values to the JSON object
+	for (let column in headers) {
+		// First check if the current property is an array string. If so, then we'll split it and map the results to an array.
+		// We trim the values to remove any whitespace.
+		if (jsonProperties[column].includes(",")) {
+			jsonObject[headers[column]] = jsonProperties[column]
 				.split(",").map(item => item.trim());
 		} else {
-			jsonObject[headers[j].replace("\r", "")] = jsonProperties[j].replace("\r", "")
+			// Otherwise, just assign the value to the JSON object.
+			// We replace \r with an empty string to remove any carriage returns.
+			jsonObject[headers[column].replace("\r", "")] = jsonProperties[column].replace("\r", "")
 		}
 
 		// This extracts only the emissions intensity data from the CSV.
 		// We use this to generate a smaller data file which can be later imported into CO2.js
-		if (headers[j].startsWith('emissions_intensity_gco2_per_kwh')) {
-			gridIntensityResults[country.toLowerCase()] = jsonProperties[j].replace("\r", "")
+		if (headers[column].startsWith('emissions_intensity_gco2_per_kwh')) {
+			gridIntensityResults[country.toLowerCase()] = jsonProperties[column].replace("\r", "")
 		}
 	}
 
 	// Ember keeps the country name in the 2nd column, so we'll use that to map the ISO country codes
-	const countryCodes = getCountryCodes(countries, jsonProperties[1].toLowerCase());
+	const countryCodes = getCountryCodes(jsonProperties[1].toLowerCase());
 
 	/* Push the genearted JSON object to resultant array */	
 	csvToJsonResult[country] = {...jsonObject, ...countryCodes};
--- data/utils/getCountryCodes.js
+++ data/utils/getCountryCodes.js
@@ -1,7 +1,12 @@
-const getCountryCodes = (countries, country) => {
-    return countries.find(findCountry => {
-		if (findCountry.country_name.toLowerCase() === country) {
-			const { country_code_iso_2, country_code_iso_3 } = findCountry;
+const mapCountries = require("./mapCountries");
+const countries = mapCountries();
+
+// Search the countries array based on an input value. We run the search on the "country_name" property.
+// If a match is found, we return the country code (2-digit and 3-digit).
+const getCountryCodes = (input) => {
+    return countries.find(country => {
+		if (country.country_name.toLowerCase() === input) {
+			const { country_code_iso_2, country_code_iso_3 } = input;
 			return { country_code_iso_2, country_code_iso_3 }
 		}
 	})
