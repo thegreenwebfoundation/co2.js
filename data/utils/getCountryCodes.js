8a6ed4453a4b683bcdaf26d6d5e730fcb3f24d68
Fershad <itsfish@fershad.com>
2022-09-08 17:55:30 +0800

    update function to map countries

--- data/utils/getCountryCodes.js
+++ data/utils/getCountryCodes.js
@@ -3,13 +3,16 @@ const countries = mapCountries();
 
 // Search the countries array based on an input value. We run the search on the "country_name" property.
 // If a match is found, we return the country code (2-digit and 3-digit).
-const getCountryCodes = (input) => {
-    return countries.find(country => {
-		if (country.country_name.toLowerCase() === input) {
+const getCountryCodes = (field = 'ember_country_name', input) => {
+    const mappedCountry = countries.find(country => {
+		if (country[field].toLowerCase() === input) {
 			const { country_code_iso_2, country_code_iso_3 } = input;
 			return { country_code_iso_2, country_code_iso_3 }
 		}
 	})
+
+	const { country_code_iso_2, country_code_iso_3 } = mappedCountry;
+	return { country_code_iso_2, country_code_iso_3 };
 }
 
 module.exports = getCountryCodes;
