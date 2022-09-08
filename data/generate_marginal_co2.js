2098491d163638338a8a3570bb18f3c01bacc111
Fershad <itsfish@fershad.com>
2022-09-08 17:21:04 +0800

    generate working output

--- data/generate_marginal_co2.js
+++ data/generate_marginal_co2.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 let csv = fs.readFileSync("data/IFI_Default_Grid_Factors_2021_v3.1_unfccc.csv");
+const parseCSVRow = require("./utils/parseCSVRow");
+const getCountryCodes = require("./utils/getCountryCodes");
 
 const array = csv.toString().split("\n");
 
@@ -8,47 +10,51 @@ const csvToJsonResult = {};
 const gridIntensityResults = {};
 
 /* Store the CSV column headers into seprate variable */
-const headers = [
-  array[4].split(",").filter((item) => item !== ""),
-  array[3].split(',"').filter((item) => item !== ""),
-].flat();
+const rowHeaders = parseCSVRow(array[4]);
+rowHeaders.push(...parseCSVRow(array[3]));
+rowHeaders.push("Operating Margin Grid Emission");
 
-headers.push("Operating Margin Grid Emission");
+const headers = rowHeaders.filter((header) => header !== "");
 
 /* Iterate over the remaining data rows */
-for (let i = 5; i < array.length - 8; i++) {
+// Here we remove the first 5 items in the array, since these are the headings which we have already accounted for
+for (let currentArrayString of array.slice(5)) {
+
+  // If there's an empty line, keep calm and carry on.
+  if (currentArrayString.length === 0) continue;
+
   /* Empty object to store result in key value pair */
   const jsonObject = {};
-  /* Store the current array element */
-  const currentArrayString = array[i];
-  let string = "";
-  let quoteFlag = 0;
-  for (let character of currentArrayString) {
-    if (character === '"' && quoteFlag === 0) {
-      quoteFlag = 1;
-    } else if (character === '"' && quoteFlag == 1) quoteFlag = 0;
-    if (character === "," && quoteFlag === 0) character = "|";
-    if (character !== '"') string += character;
-  }
 
-  let jsonProperties = string.split("|");
+	// Split the string by the pipe character
+  let jsonProperties = parseCSVRow(currentArrayString);
+
+  if (jsonProperties.length < 2) continue;
+
+  // The UNFCCC data sets the country value (full country name as string) in the first column, so we can extract that.
   let country = jsonProperties[0];
-  console.log(country, jsonProperties);
 
+  // If there's no value for the country, then we can skip this row.
   if (!country || country === "") continue;
 
-  for (let j in headers) {
-    if (jsonProperties[j].includes(",")) {
-      jsonObject[headers[j]] = jsonProperties[j]
+  // Loop through the headers and assign the values to the JSON object
+  for (let column in headers) {
+    if (!column || column === "") continue;
+
+    console.log(headers[column], jsonProperties[column]);
+    // First check if the current property is an array string. If so, then we'll split it and map the results to an array.
+		// We trim the values to remove any whitespace.
+    if (jsonProperties[column].includes(",")) {
+      jsonObject[headers[column]] = jsonProperties[column]
         .split(",")
         .map((item) => item.trim());
     } else {
-      jsonObject[headers[j].replace("\r", "").replaceAll('\"', "")] =
-        jsonProperties[j].replace("\r", "").replace('\"', "");
+      jsonObject[headers[column].replace("\r", "").replaceAll('\"', "")] =
+        jsonProperties[column].replace("\r", "").replace('\"', "");
     }
 
-    if (headers[j].startsWith("Operating Margin Grid Emission")) {
-      gridIntensityResults[country.toLowerCase()] = jsonProperties[j]
+    if (headers[column].startsWith("Operating Margin Grid Emission")) {
+      gridIntensityResults[country.toLowerCase()] = jsonProperties[column]
         .replace("\r", "")
         .replace('\"', "");
     }
