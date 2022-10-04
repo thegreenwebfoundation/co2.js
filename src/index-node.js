9cf53eb5f784700656a28c0a3500408ec3363856
Fershad <itsfish@fershad.com>
2022-10-04 11:24:53 +0800

    update imports

--- src/index-node.js
+++ src/index-node.js
@@ -1,6 +1,5 @@
 import co2 from "./co2.js";
 import hosting from "./hosting.js";
-import averageIntensity from "./data/average-intensities-ember-2021.min.js";
-import marginalIntensity from "./data/marginal-intensities-unfccc-2021.min.js";
-
+import averageIntensity from "./data/average-intensities-2021.min.js";
+import marginalIntensity from "./data/marginal-intensities-2021.min.js";
 export { co2, hosting, averageIntensity, marginalIntensity };
