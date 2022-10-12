c966044737ab2988f5d9e513453c40eb9bd9b31a
soulgalore <peter@soulgalore.com>
2022-10-12 15:53:05 +0200

    Fix correct export for hosting for Node.
    https://github.com/thegreenwebfoundation/co2.js/issues/110
    https://github.com/sitespeedio/sitespeed.io/issues/3735

--- src/index-node.js
+++ src/index-node.js
@@ -1,5 +1,5 @@
 import co2 from "./co2.js";
-import hosting from "./hosting.js";
+import hosting from "./hosting-node.js";
 import averageIntensity from "./data/average-intensities-2021.min.js";
 import marginalIntensity from "./data/marginal-intensities-2021.min.js";
 export { co2, hosting, averageIntensity, marginalIntensity };
