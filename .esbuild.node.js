641ae847723d5e977fa6e11f529a5a62a6988f8b
Fershad Irani <27988517+fershad@users.noreply.github.com>
2023-01-19 12:03:48 +0800

    fix missing 1byte build

--- .esbuild.node.js
+++ .esbuild.node.js
@@ -2,7 +2,7 @@ const { globPlugin } = require('esbuild-plugin-glob');
 
 function main() {
   require('esbuild').build({
-    entryPoints: ['src/**/**[!test.js].js',],
+    entryPoints: ['src/**/!(*.test.js|!(*.js))',],
     bundle: false,
     minify: false,
     sourcemap: true,
