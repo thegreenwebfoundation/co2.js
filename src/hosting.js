4532de30030231272d54ef53741407038528f5c9
fershad <27988517+fershad@users.noreply.github.com>
2023-05-18 09:21:13 +0800

    remove unused db

--- src/hosting.js
+++ src/hosting.js
@@ -5,10 +5,9 @@ import hostingAPI from "./hosting-api.js";
 /**
  * Check if a domain is hosted by a green web host.
  * @param {string|array} domain - The domain to check, or an array of domains to be checked.
- * @param {object} db - OPTIONAL. A copy of the Green Web Dataset. If this is not provided, checks will be performed against the Green Web Foundation API.
  * @returns {boolean|array} - A boolean if a string was provided, or an array of booleans if an array of domains was provided.
  */
-function check(domain, db) {
+function check(domain) {
   return hostingAPI.check(domain);
 }
 
