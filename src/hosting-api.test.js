00aa63b833e3af4879ef661d7475916bfb6b98ef
fershad <27988517+fershad@users.noreply.github.com>
2024-01-23 17:31:52 +0800

    remove nock

--- src/hosting-api.test.js
+++ src/hosting-api.test.js
@@ -34,18 +34,6 @@ describe("hostingAPI", () => {
   });
   describe("implicitly checking multiple domains with #check", () => {
     it.skip("using the API", async () => {
-      const scope = nock("https://api.thegreenwebfoundation.org/")
-        .get("/v2/greencheckmulti/[%22google.com%22,%22kochindustries.com%22]")
-        .reply(200, {
-          "google.com": {
-            url: "google.com",
-            green: true,
-          },
-          "kochindustries.com": {
-            url: "kochindustries.com",
-            green: null,
-          },
-        });
       const res = await hosting.check(["google.com", "kochindustries.com"]);
       expect(res).toContain("google.com");
     });
