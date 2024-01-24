df02bc3aab9b1e15367c6c1f1a9b470ddd925638
fershad <27988517+fershad@users.noreply.github.com>
2024-01-24 12:39:58 +0800

    use mocks for api tests

--- src/hosting-api.test.js
+++ src/hosting-api.test.js
@@ -1,40 +1,66 @@
 "use strict";
 
-import hosting from "./hosting-node.js";
-import https from "https";
+import hosting from "./hosting-api.js";
 /* eslint-disable jest/no-disabled-tests */
 
 process.env.CO2JS_VERSION = "1.2.34";
 const requestHeaderComment = "TestRunner";
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ green: true }),
+  })
+);
+
 describe("hostingAPI", () => {
-  let httpsGetSpy;
   beforeEach(() => {
-    httpsGetSpy = jest.spyOn(https, "get");
-  });
-  afterEach(() => {
-    jest.restoreAllMocks();
+    fetch.mockClear();
   });
   describe("checking a single domain with #check", () => {
-    it.skip("using the API", async () => {
-      const res = await hosting.check("google.com", null, requestHeaderComment);
+    it("using the API", async () => {
+      const res = await hosting.check("google.com");
+      expect(fetch).toHaveBeenCalledTimes(1);
       expect(res).toEqual(true);
     });
     it("sets the correct user agent header", async () => {
-      const res = await hosting.check("google.com", null, requestHeaderComment);
-      expect(httpsGetSpy).toHaveBeenCalledTimes(1);
-      expect(httpsGetSpy).toHaveBeenLastCalledWith(
+      const res = await hosting.check("google.com", requestHeaderComment);
+
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenLastCalledWith(
         expect.any(String),
         expect.objectContaining({
           headers: { "User-Agent": "co2js/1.2.34 TestRunner" },
-        }),
-        expect.any(Function)
+        })
       );
+      expect(res).toEqual(true);
     });
   });
   describe("implicitly checking multiple domains with #check", () => {
-    it.skip("using the API", async () => {
+    it("using the API", async () => {
+      fetch.mockImplementation(() =>
+        Promise.resolve({
+          json: () =>
+            Promise.resolve({
+              "google.com": { url: "google.com", green: true },
+            }),
+        })
+      );
       const res = await hosting.check(["google.com", "kochindustries.com"]);
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(res).toContain("google.com");
+    });
+    it("sets the correct user agent header", async () => {
+      const res = await hosting.check(
+        ["google.com", "kochindustries.com"],
+        requestHeaderComment
+      );
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenLastCalledWith(
+        expect.any(String),
+        expect.objectContaining({
+          headers: { "User-Agent": "co2js/1.2.34 TestRunner" },
+        })
+      );
       expect(res).toContain("google.com");
     });
   });
