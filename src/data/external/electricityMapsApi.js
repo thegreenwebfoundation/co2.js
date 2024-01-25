c2c063de180806f462bbaf0552607d49e3481690
fershad <27988517+fershad@users.noreply.github.com>
2024-01-25 22:12:33 +0800

    initial implementation for current, historic, and zones endpoints

--- src/data/external/electricityMapsApi.js
+++ src/data/external/electricityMapsApi.js
@@ -1,20 +1,45 @@
 class ElectricityMapsApi {
-  constructor() {
-    this.baseUrl = "https://api.electricitymap.org/v3";
+  constructor(options) {
+    this.baseUrl = "https://api-access.electricitymaps.com/free-tier/";
+    this.apiKey = options.apiKey;
   }
 
-  async get(countryCode) {
-    // const url = `${this.baseUrl}/country/latest/${countryCode}`;
-    // const response = await fetch(url);
-    // const data = await response.json();
-    return { countryCode };
+  async get(zone, lat, lon) {
+    const url = `${this.baseUrl}/carbon-intensity/latest?lat=${lat}&lon=${lon}&zone=${zone}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return { data };
+  }
+
+  async getHistory(zone, lat, lon, dataTime = undefined) {
+    const url = `${this.baseUrl}/carbon-intensity/history?lat=${lat}&lon=${lon}&zone=${zone}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.status === "error") {
+      throw new Error(data.message);
+    }
+
+    if (dataTime) {
+      try {
+        return data.history.filter((d) => d.datetime === dataTime)[0];
+      } catch {
+        throw new Error(
+          `No data for ${dataTime} in ${
+            (zone | ("lat: " + lat), "lon: " + lon)
+          }`
+        );
+      }
+    }
+
+    return data.history;
   }
 
   async getZones() {
-    // const url = `${this.baseUrl}/zones`;
-    // const response = await fetch(url);
-    // const data = await response.json();
-    return ["a", "b", "c"];
+    const url = `${this.baseUrl}/zones`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
   }
 }
 
