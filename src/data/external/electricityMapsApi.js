e76e2b86acfc793fd10d9b21030013251e8615db
fershad <27988517+fershad@users.noreply.github.com>
2024-01-27 22:14:50 +0800

    update request URL

--- src/data/external/electricityMapsApi.js
+++ src/data/external/electricityMapsApi.js
@@ -1,19 +1,51 @@
 class ElectricityMapsApi {
   constructor(options) {
-    this.baseUrl = "https://api-access.electricitymaps.com/free-tier/";
-    this.apiKey = options.apiKey;
+    this.baseUrl = "https://api-access.electricitymaps.com/free-tier";
+    this.authToken = options?.authToken || undefined;
   }
 
   async getLatest(zone, lat, lon) {
-    const url = `${this.baseUrl}/carbon-intensity/latest?lat=${lat}&lon=${lon}&zone=${zone}`;
-    const response = await fetch(url);
+    if (!this.authToken || this.authToken === undefined) {
+      throw new Error(
+        "An authentication token is required to access this endpoint."
+      );
+    }
+
+    const query = `${lat ? `lat=${lat}&` : ""}${lon ? `lon=${lon}&` : ""}${
+      zone ? `zone=${zone}` : ""
+    }`;
+    const url = `${this.baseUrl}/carbon-intensity/latest?${query}`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "auth-token": this.authToken,
+      },
+    });
     const data = await response.json();
+
+    if (data.status === "error") {
+      throw new Error(data.message);
+    }
+
     return { data };
   }
 
   async getHistory(zone, lat, lon, dataTime = undefined) {
-    const url = `${this.baseUrl}/carbon-intensity/history?lat=${lat}&lon=${lon}&zone=${zone}`;
-    const response = await fetch(url);
+    if (!this.authToken || this.authToken === undefined) {
+      throw new Error(
+        "An authentication token is required to access this endpoint."
+      );
+    }
+    const query = `${lat ? `lat=${lat}&` : ""}${lon ? `lon=${lon}&` : ""}${
+      zone ? `zone=${zone}` : ""
+    }`;
+    const url = `${this.baseUrl}/carbon-intensity/history?${query}`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "auth-token": this.authToken,
+      },
+    });
     const data = await response.json();
 
     if (data.status === "error") {
