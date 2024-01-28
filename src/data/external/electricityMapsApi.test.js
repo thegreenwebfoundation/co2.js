75b9ce1bbf8346294356e5a4c1aea86e2f6fb417
fershad <27988517+fershad@users.noreply.github.com>
2024-01-28 21:36:33 +0800

    remove mocks file

--- src/data/external/electricityMapsApi.test.js
+++ src/data/external/electricityMapsApi.test.js
@@ -1,10 +1,32 @@
 "use strict";
 
 import ElectricityMapApi from "./electricityMapsApi.js";
-import {
-  taiwanHistoricalData,
-  taiwanLatestData,
-} from "../../../__mocks__/electricityMapsApi.js";
+
+const taiwanHistoricalData = {
+  history: [
+    {
+      zone: "TW",
+      carbonIntensity: 492,
+      datetime: "2024-01-27T13:00:00.000Z",
+      updatedAt: "2024-01-27T12:47:05.424Z",
+      createdAt: "2024-01-24T13:49:22.390Z",
+      emissionFactorType: "lifecycle",
+      isEstimated: true,
+      estimationMethod: "TIME_SLICER_AVERAGE",
+    },
+  ],
+};
+
+const taiwanLatestData = {
+  zone: "TW",
+  carbonIntensity: 492,
+  datetime: "2024-01-27T13:00:00.000Z",
+  updatedAt: "2024-01-27T12:47:05.424Z",
+  createdAt: "2024-01-24T13:49:22.390Z",
+  emissionFactorType: "lifecycle",
+  isEstimated: true,
+  estimationMethod: "TIME_SLICER_AVERAGE",
+};
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
