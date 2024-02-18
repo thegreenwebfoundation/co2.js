031b83c4834dcaf63e188009e2c61d069fe5ec86
Simon Fishel <simonf@gmail.com>
2024-02-18 08:42:22 -0800

    adds support for verbose response when chacking domains
    - adds a new `checkVerbose` function to both hosting-node.js and hosting-api.js
    - for checking a single domain, `checkVerbose` returns the entire payload from the api.thegreenwebfoundation.org endpoint
    - for checking multiple domains, `checkVerbose` returns a dictionary of each domain mapped to its response payload
    
    Fixes #189

--- src/hosting-json.node.js
+++ src/hosting-json.node.js
@@ -45,63 +45,6 @@ async function loadJSON(jsonPath) {
   return JSON.parse(jsonBuffer);
 }
 
-/**
- * Check if a string or array of domains has been provided
- * @param {string|array} domain - The domain to check, or an array of domains to be checked.
- */
-async function check(domain, db) {
-  // is it a single domain or an array of them?
-  if (typeof domain === "string") {
-    return checkInJSON(domain, db);
-  } else {
-    return checkDomainsInJSON(domain, db);
-  }
-}
-
-/**
- * Check if a domain is hosted by a green web host by querying the database.
- * @param {string} domain - The domain to check.
- * @param {object} db - The database to check against.
- * @returns {boolean} - A boolean indicating whether the domain is hosted by a green web host.
- */
-function checkInJSON(domain, db) {
-  if (db.indexOf(domain) > -1) {
-    return true;
-  }
-  return false;
-}
-
-/**
- * Extract the green domains from the results of a green check.
- * @param {object} greenResults - The results of a green check.
- * @returns {array} - An array of domains that are hosted by a green web host.
- */
-function greenDomainsFromResults(greenResults) {
-  const entries = Object.entries(greenResults);
-  const greenEntries = entries.filter(([key, val]) => val.green);
-
-  return greenEntries.map(([key, val]) => val.url);
-}
-
-/**
- * Check if an array of domains is hosted by a green web host by querying the database.
- * @param {array} domains - An array of domains to check.
- * @param {object} db - The database to check against.
- * @returns {array} - An array of domains that are hosted by a green web host.
- */
-function checkDomainsInJSON(domains, db) {
-  let greenDomains = [];
-
-  for (let domain of domains) {
-    if (db.indexOf(domain) > -1) {
-      greenDomains.push(domain);
-    }
-  }
-  return greenDomains;
-}
-
 module.exports = {
-  check,
   loadJSON,
-  greenDomainsFromResults,
 };
