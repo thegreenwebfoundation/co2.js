be68fe68d1dbcd19d719108ac6e6bf611e718457
fershad <27988517+fershad@users.noreply.github.com>
2026-06-29 15:10:25 +0800

    esbuild update

--- .esbuild.esm.js
+++ .esbuild.esm.js
@@ -1,16 +1,18 @@
-const esbuild = require('esbuild')
+const esbuild = require("esbuild");
 // tiny glob is a dependency of `esbuild-plugin-glob`.
 // For this build however we need to filter out some extra files
 // that are used for nodejs, but not in browsers, so we use the
 // library directly instead of using `esbuild-plugin-glob` as a plugin
-const glob = require('tiny-glob')
-const esbuildCommon = require('./.esbuild.common')
+const glob = require("tiny-glob");
+const esbuildCommon = require("./.esbuild.common");
 
 async function main() {
-  const results = await glob('src/**/!(*.test.js|test-constants.js|!(*.js))')
+  const results = await glob("src/**/!(*.test.js|test-constants.js|!(*.js))");
   // we remove node specific files here, with the assumption that
   // the common use case is bundling into browser based web apps
-  const justBrowserCompatibleFiles = results.filter(filepath => !filepath.endsWith('node.js'))
+  const justBrowserCompatibleFiles = results.filter(
+    (filepath) => !filepath.endsWith("node.js")
+  );
 
   esbuild.build({
     ...esbuildCommon,
@@ -18,10 +20,13 @@ async function main() {
     bundle: false,
     minify: false,
     sourcemap: false,
-    target: ['chrome58', 'firefox57', 'safari11', 'edge18', 'esnext'],
-    outdir: 'dist/esm',
-    outExtension: { '.js': '.js' },
-    format: 'esm'
-  })
+    target: ["chrome58", "firefox57", "safari11", "edge18", "esnext"],
+    outdir: "dist/esm",
+    outExtension: { ".js": ".js" },
+    format: "esm",
+    supported: {
+      destructuring: true,
+    },
+  });
 }
-main()
+main();
